febc0672e965a0f7342042baee34938c8ac265e5
Jasper Ginn <jasperginn@gmail.com>
2018-08-04 21:15:22 +0200

    initial commit

---
+++ pararius/main.R
@@ -0,0 +1,17 @@
+##############################
+#
+# Scrape pararius new rentals
+#  in The Hague and store in
+#  sqlite database
+#
+#############################
+
+## Initialize 
+
+# Log 
+
+## Scrape results, filter & store in database
+
+# Apartment url is primary key
+
+## Mail interesting results
---
+++ pararius/settings.R
@@ -0,0 +1,15 @@
+# Settings
+
+options("PARARIUS_SEARCH_URL" = "https://www.pararius.nl/huurwoningen/den-haag/250-700")
+
+# Headers
+options("PARARIUS_HEADERS" = c(
+  'User-Agent' = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+  'Connection' = 'keep-alive',
+  'Accept-Language' = 'en-GB,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,en-US;q=0.2,fr;q=0.2,zh-TW;q=0.2',
+  'Accept-Encoding' = 'gzip, deflate, br',
+  'Accept' = '*/*',
+  'Accept-Charset' = 'GBK,utf-8;q=0.7,*;q=0.3',
+  'Cache-Control' = 'max-age=0'
+  )
+)
